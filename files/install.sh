11f93394c8196c0a1836f51340dca1000b8931c7
macwinnie <dev@macwinnie.me>
2021-08-24 18:55:12 +0000

    fix python packages that caused installation to fail

--- files/install.sh
+++ files/install.sh
@@ -20,8 +20,8 @@ apt-get -yq upgrade
 echo
 echo -e '\033[1;30;42m install needed tools \033[0m'
 apt-get -yq install -y --no-install-recommends \
-        python-setuptools python-pip python-pkg-resources \
-        python-jinja2 python-yaml \
+        python3-setuptools python3-pip python3-pkg-resources \
+        python3-jinja2 python3-yaml \
         vim nano \
         htop tree tmux screen sudo git zsh ssh screen \
         supervisor expect \
